e584e8c9c732ce1210f4953cd632b0126c5b39e8
eshoiza <engrjayt200@gmail.com>
2020-06-08 11:47:19 +0100

    updated app component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import UserForm from "./components/UserForm";
+ function App() {
+  return (
+    <div className="container">
+      <UserForm />
+    </div>
+  )
+}
+export default App;
